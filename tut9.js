edeba05ba666881214581a9b6c9cb383a71180b6
amit-arya <amit3128arya@gmail.com>
2022-11-02 22:16:20 +0530

    Add files via upload

--- tut9.js
+++ tut9.js
@@ -1,21 +1,46 @@
-/*let multiply = function(x,y){
-  console.log(x*y);
-}
-
-let multiplyByTwo = multiply.bind(this, 2);
-multiplyByTwo(5);
-
-let multiplyByThree = multiply.bind(this, 3);
-multiplyByThree(5);*/
-
-let multiply = function(x){
-    return function(y){
-      console.log(x*y);
-    }
+class Student{
+  static count = 0;
+  constructor(name, age, phone_number, board_marks){
+    this.name = name;
+    this.age = age;
+    this.phone_number = phone_number;
+    this.board_marks = board_marks;
+    Student.countStudents();
   }
   
-  let multiplyByTwo = multiply(2);
-  multiplyByTwo(5);
+  //count the number of objects created----------------
   
-  let multiplyByThree = multiply(3);
-  multiplyByThree(5);
+  static countStudents(){
+    return Student.count++;
+  }
+  checkEligible(){
+    if(board_marks > 40)
+      console.log('Eligible');
+    else
+      console.log('Not eligible');
+  }
+
+  eligibleforplacements(min_marks){
+     return (min_age)=>{
+         if(this.board_marks > min_marks && this.age > min_age)
+           return true;
+         else
+         return false;  
+     }
+  }
+  
+}
+
+var stud1 = new Student('Amit', 23, 24224234, 90);
+var stud2 = new Student('Satish', 21, 21827516, 80);
+var stud3 = new Student('Ankur', 26, 45346653, 53);
+var stud4 = new Student('Anil', 27, 45243244, 63);
+var stud5 = new Student('Alok', 19, 34535645, 97);
+
+console.log(stud1.eligibleforplacements(60)(20));
+console.log(stud2.eligibleforplacements(60)(20));
+console.log(stud3.eligibleforplacements(60)(20));
+console.log(stud4.eligibleforplacements(60)(20));
+console.log(stud5.eligibleforplacements(60)(20));
+
+//console.log(Student.countStudents());
